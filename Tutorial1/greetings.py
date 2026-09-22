b9d3e9f193953a765ae47471d8ea1d1f4be19beb
Yang Haozhe <yhz2004@connect.hku.hk>
2026-09-22 11:34:53 +0800

    tutorial 2 optional reading

---
+++ Tutorial1/greetings.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+print("Hello,", name)
+print("Next year you will be", age + 1)
---
+++ Tutorial1/temperature.py
@@ -0,0 +1,3 @@
+celsius = int(input("Temperature in Celsius? "))
+fahrenheit = ...        # your formula here
+print("That is", fahrenheit, "degrees Fahrenheit")
